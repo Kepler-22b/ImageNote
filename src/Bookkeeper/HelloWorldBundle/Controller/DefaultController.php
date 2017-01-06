15177092e78ad628efd843e784b16153dce4af4b
Kepler-22b <andriy@siteimage.com.ua>
2017-01-06 23:31:52 +0200

    Add Bookkeeper/ManageBundle and output Hello world

--- src/Bookkeeper/HelloWorldBundle/Controller/DefaultController.php
+++ src/Bookkeeper/HelloWorldBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig');
+    public function indexAction($name) {
+        return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig', [
+            'name' => $name
+        ]);
     }
 }
