a8391e4705b8863c1b24f864d93eb2f451f3ed70
Kepler-22b <andriy@siteimage.com.ua>
2017-01-17 23:44:16 +0200

    Create Book action

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -3,7 +3,7 @@
 namespace Bookkeeper\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Bookkeeper\ManagerBundle\Entity\Book;
 use Bookkeeper\ManagerBundle\Form\BookType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -27,7 +27,7 @@ class BookController extends Controller
     {
         $book = new Book();
 
-        $form = $this->createForm(BookType::class , $book, [
+        $form = $this->createForm(BookType::class, $book, [
             'action' => $this->generateUrl('book_create'),
             'method' => 'POST'
         ]);
@@ -40,6 +40,31 @@ class BookController extends Controller
 
     public function createAction(Request $request)
     {
+        $book = new Book();
+
+        $form = $this->createForm(BookType::class, $book, [
+            'action' => $this->generateUrl('book_create'),
+            'method' => 'POST'
+        ]);
+        $form->add('submit', SubmitType::class, ['label' => 'Create Book']);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('msg', 'Your book has been created');
+
+            return $this->redirect($this->generateUrl('book_new'));
+        }
+
+        $this->get('session')->getFlashBag()->add('msg', 'Something went wrong');
+
+        return $this->render('BookkeeperManagerBundle:Book:new.html.twig', [
+            'form' => $form->createView()
+        ]);
 
     }
 
