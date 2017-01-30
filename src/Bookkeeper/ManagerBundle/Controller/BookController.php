341646f3d1eddcfd14c0b2323a51b511f08250e3
Kepler-22b <andriy@siteimage.com.ua>
2017-01-30 08:01:42 +0200

    [Manager Bundle] Edit book action

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -78,15 +78,52 @@ class BookController extends Controller
 
     public function editAction($id)
     {
-        return $this->render('BookkeeperManagerBundle:Book:edit.html.twig');
+		$em = $this->getDoctrine()->getManager();
+		$book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
+
+	    $form = $this->createForm(BookType::class, $book, array(
+		    'action' => $this->generateUrl('book_update', array('id' => $book->getId())),
+		    'method' => 'PUT'
+	    ));
+
+	    $form->add('submit', SubmitType::class, array('label' => 'Update Book'));
+
+	    return $this->render('BookkeeperManagerBundle:Book:edit.html.twig', array(
+		    'form' => $form->createView(),
+		    'book' => $book
+	    ));
     }
 
     public function updateAction(Request $request, $id)
     {
+	    $em = $this->getDoctrine()->getManager();
+	    $book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
 
+	    $form = $this->createForm(BookType::class, $book, array(
+		    'action' => $this->generateUrl('book_update', array('id' => $book->getId())),
+		    'method' => 'PUT'
+	    ));
+
+	    $form->add('submit', SubmitType::class, array('label' => 'Update Book'));
+
+	    //обробка запиту що передається параметром Request $request
+	    $form->handleRequest($request);
+
+	    //якщо валідно, запис в БД та редірект на сторінку запису
+	    if ($form->isValid()){
+		    $em->flush();
+		    $this->get('session')->getFlashBag()->add('msg', 'Book Updated');
+		    return $this->redirect($this->generateUrl('book_show', array('id' => $id)));
+	    }
+
+	    //якщо не валідно повернення на форму з повідомелння про валідацію
+	    return $this->render('BookkeeperManagerBundle:Book:edit.html.twig', array(
+		    'form' => $form->createView(),
+		    'book' => $book
+	    ));
     }
 
-    public function deleteAction($id)
+    public function deleteAction(Request $request, $id)
     {
 
     }
