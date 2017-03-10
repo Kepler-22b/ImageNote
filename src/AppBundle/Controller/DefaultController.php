6c6fca222edb1979f7d9d5b6f7e6248ea3edcbf4
Kepler-22b <andriy@siteimage.com.ua>
2017-03-10 08:39:32 +0200

    [Symfony] render array of latest posts in index action

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,13 +2,21 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Note;
 
 class DefaultController extends Controller {
 
 	public function indexAction() {
-		return $this->render( 'AppBundle::index.html.twig' );
+		$em = $this->getDoctrine()->getManager();
+
+		$latestPosts = $em->createQueryBuilder()
+			->select("u")
+			->from(Note::class, "u")
+			->addOrderBy("u.dateCreated", "DESC")
+			->getQuery()
+			->getResult();
+
+		return $this->render( "AppBundle::index.html.twig", ["latestPosts" => $latestPosts] );
 	}
 }
